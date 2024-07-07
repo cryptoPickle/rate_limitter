d87ccbd798a9b0fefb3d9ea278db589f30582a8a
Cryptopickle <halilibrahimirmak@gmail.com>
2024-07-07 22:31:25 +0100

    example rate limitter

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 	"time"
+
+	ratelimitter "github.com/cryptoPickle/rate_limitter/pkg/rate_limitter"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
+type Server struct {
+	*gin.Engine
+	*ratelimitter.RateLimit
+}
+
+func NewServer(r *ratelimitter.RateLimit) *Server {
+	s := gin.Default()
+	return &Server{
+		Engine:    s,
+		RateLimit: r,
+	}
+}
+
+func (s *Server) Router() {
+	s.GET("/test", RateLimitterMiddleWare(s.RateLimit),
+		func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "ok",
+			})
+		})
+}
+
+func RateLimitterMiddleWare(rl *ratelimitter.RateLimit) func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		clientIp := ctx.ClientIP()
+		if err := rl.Start(clientIp, ctx); err != nil {
+			ctx.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{"message": "rate limit exceeded"})
+			return
+		}
+	}
+}
+
 func main() {
-	duration := time.Second * 2
-
-	ticker := time.NewTicker(duration)
-
-	for {
-		<-ticker.C
-		fmt.Println("tick")
+	rl := ratelimitter.New(10, time.Second*1)
+	s := NewServer(rl)
+	s.Router()
+	if err := s.Run(":8080"); err != nil {
+		logrus.Fatal(err)
 	}
 }
--- pkg/rate_limitter/rate.go
+++ pkg/rate_limitter/rate.go
@@ -1,47 +1,74 @@
 package ratelimitter
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Nexter interface {
 	Next()
-	AbortWithStatusJSON(int, any)
 }
 
 type RateLimit struct {
-	exitch chan struct{}
-	bucket map[string]int
-	Nexter
+	exitch   chan struct{}
+	ipch     chan string
+	errorch  chan error
+	bucket   map[string]int
 	duration time.Duration
 	capacity int
 }
 
-func New(capacity int, duration time.Duration, next Nexter) *RateLimit {
+func New(capacity int, duration time.Duration) *RateLimit {
 	r := &RateLimit{
 		bucket:   make(map[string]int),
+		exitch:   make(chan struct{}),
+		errorch:  make(chan error),
+		ipch:     make(chan string),
 		capacity: capacity,
 		duration: duration,
 	}
+
 	go r.refill()
 	return r
 }
 
-func (r *RateLimit) Start() {
+func (r *RateLimit) Start(ip string, nexter Nexter) error {
+	r.ipch <- ip
+	if err := <-r.errorch; err != nil {
+		return err
+	}
+
+	nexter.Next()
+	return nil
 }
 
 func (r *RateLimit) Stop() {
 	close(r.exitch)
 }
 
-func (r *RateLimit) refill(ip string) {
+func (r *RateLimit) refill() {
 	ticker := time.NewTicker(r.duration)
 
 	for {
 		select {
 		case <-ticker.C:
-			if r.bucket[ip] > r.capacity && r.bucket[ip] != 0 {
-				r.bucket[ip] = r.bucket[ip] - 1
+			for key, val := range r.bucket {
+				if val > 0 {
+					r.bucket[key]--
+				}
+			}
+		case val := <-r.ipch:
+			if _, exists := r.bucket[val]; !exists {
+				r.bucket[val] = 0
+			}
+			if r.bucket[val] >= r.capacity {
+				r.errorch <- fmt.Errorf("rate limit exceed")
+			} else {
+				r.bucket[val]++
+				r.errorch <- nil
 			}
 		case <-r.exitch:
+			ticker.Stop()
 			return
 		}
 	}
